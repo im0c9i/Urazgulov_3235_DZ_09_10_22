6f4e090e95adb70d20c4e4847bcfe355e8d4fc7c
Ramil <ramil-urazgulov444@mail.ru>
2022-10-07 22:59:27 +0700

    kvadrat_chisla

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */ 
+
+void GetNumber(int number)
+{
+    Console.Write ($"{number} ->");
+
+    for (int i = 1; i <= number; i++)
+    {
+    int kvadrat = i*i*i;
+    Console.Write($" {kvadrat}, ");
+
+    }
+}
+System.Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine());
+GetNumber(num);
